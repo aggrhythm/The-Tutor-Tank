b46a0c16aae84819eaf68ff071c4ac907a123678
Rhytham Aggarwal <aggarwalrhythm2003@gmail.com>
2024-05-02 22:31:00 +0530

    Update

--- src/components/Header-Footer/footer.jsx
+++ src/components/Header-Footer/footer.jsx
@@ -9,7 +9,7 @@ function Footer() {
           &copy; {new Date().getFullYear()} TutorTank. All rights reserved.
         </p>
         <p className="text-sm mt-2">
-          Disclaimer: This website is for demonstration of TutorTank and does not represent real services or offers. It is intended solely for showcasing the capabilities of the EduMate platform. Any resemblance to actual products, services, or events is purely coincidental
+          Disclaimer: This website is for demonstration of TutorTank and does not represent real services or offers. It is intended solely for showcasing the capabilities of the TutorTank platform. Any resemblance to actual products, services, or events is purely coincidental
         </p>
         <div className="mt-4">
           <a href="#" className="text-gray-300 hover:text-white mr-4">About Us</a>
